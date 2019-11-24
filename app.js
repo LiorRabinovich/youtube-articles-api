eadad8b8c9d2c6fe6e8617492dea3b91e5b5fe5b
Lior Rabinovich <lior.px@gmail.com>
2019-11-24 17:50:49 +0200

    Middlewares && What is cors

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Hello World 4'
     })
 });
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;

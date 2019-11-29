095463792300facbb787e2342b3d98b36e57e18a
Lior Rabinovich <lior.px@gmail.com>
2019-11-29 12:04:26 +0200

    Get client body

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const morgan = require('morgan');
 
 app.use(morgan("dev"));
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if (req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status(200).json({});
@@ -17,6 +22,12 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Hello World 4'
+    })
+});
+
+app.post('/articles', (req, res) => {
+    res.status(200).json({
+        body: req.body
     })
 });
 
